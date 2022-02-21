5a115d47ca282979964ec0ad0e44b0c576c202a5
Mahfod <mahfod.dev@gmail.com>
2022-02-21 17:12:32 +0100

    add Dark mode

--- src/Components/NavBar/BrightMode.js
+++ src/Components/NavBar/BrightMode.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import DarkModeToggle from 'react-dark-mode-toggle'
 
-export default function BrightMode() {
-  return (
-    <button>S</button>
-  )
+const getStorageTheme = () => {
+	let theme = 'light-theme'
+	if (localStorage.getItem('theme')) {
+		theme = localStorage.getItem('theme')
+	}
+	return theme
 }
 
+const BrightMode = () => {
+	const [theme, setTheme] = useState(getStorageTheme())
+	const [isDarkMode, setIsDarkMode] = useState(() => false)
 
+	const toggleTheme = () => {
+		if (theme === 'light-theme') {
+			setTheme('dark-theme')
+		} else {
+			setTheme('light-theme')
+		}
+	}
+
+	useEffect(() => {
+		document.documentElement.className = theme
+		localStorage.setItem('theme', theme)
+	}, [theme])
+	return (
+		<div onClick={toggleTheme} className='nav-center'>
+			{/* <button type='checkbox' className='btn' onClick={toggleTheme}>
+				</button> */}
+			<DarkModeToggle
+				checked={isDarkMode}
+				onChange={setIsDarkMode}
+				className='btn'
+				size={50}
+			/>
+		</div>
+	)
+}
+
+export default BrightMode
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Logo from "./Logo"
-import Links from "./Links"
-import BrightMode from "./BrightMode"
-import "./NavBar.css"
+import Logo from './Logo'
+import Links from './Links'
+import BrightMode from './BrightMode'
+import './NavBar.css'
 
 export default function NavBar() {
-  return (
-    <nav>
-      <div className='container-navbar'>
-        <Logo />
-        <Links />
-        <BrightMode />
-      </div> 
-    </nav>
-  )
+	return (
+		<nav>
+			<div className='container-navbar'>
+				<Logo />
+				<Links />
+				<BrightMode />
+			</div>
+		</nav>
+	)
 }
