e33fee9c33edaa13580a6000330172eb25848755
v-andreeva <veneta-andreeva_student2021@wilder.school>
2022-02-27 22:55:33 +0100

    adding style footer and update navbar and font family

--- src/Components/NavBar/Links.js
+++ src/Components/NavBar/Links.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from './NavBar.module.css'
 
 export default function Links() {
 	return (
-		<div className='container-links'>
+		<div className={style.containerLinks}>
 			<ul>
-				<Link to='/'>Accueil</Link>
-				<Link to='/apropos'>A propos</Link>
-				<Link to='/auth'>Login</Link>
-				<Link to='/quizz'>Testez-vous</Link>
+				<Link to='/'><h3>Accueil</h3></Link>
+				<Link to='/apropos'><h3>A propos</h3></Link>
+				<Link to='/auth'><h3>Login</h3></Link>
+				<Link to='/quizz'><h3>Testez-vous</h3></Link>
 			</ul>
 		</div>
 	)
