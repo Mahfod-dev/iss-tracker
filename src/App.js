1c4bca9a9f2e0027e91c127454ceacfa176473c9
Mahfod <mahfod.dev@gmail.com>
2022-03-01 21:14:31 +0100

    add context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header, Footer } from './Constants/exportModule'
+import { useGlobalContext } from './Components/Coordinates'
 
 const App = () => {
 	return (
 		<>
 			<Header />
+
 			<Footer />
 		</>
 	)
--- src/Constants/exportModule.js
+++ src/Constants/exportModule.js
@@ -1,5 +1,4 @@
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
-import Coordinates from '../Components/Coordinates'
 
-export { Header, Footer, Coordinates }
+export { Header, Footer }
