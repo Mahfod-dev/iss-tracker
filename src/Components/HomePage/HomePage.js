ddf4d777fb049496ee00c9f1f487c72b492f3628
Matthieu Mary <matthieu.mary2@gmail.com>
2022-03-05 15:02:28 +0100

    remove div from HomePage.js

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -3,9 +3,9 @@ import style from './HomePage.module.css'
 
 export default function HomePage() {
 	return (
-		<div className={style.container_map}>
+		
 			<p>merci de vous connecter pour acceder a la carte</p>
-		</div>
+		
 		
 	)
 }
