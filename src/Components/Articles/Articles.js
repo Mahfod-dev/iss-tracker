98380fbfc19b343b480f138aa627967942951a7b
Mahfod <mahfod.dev@gmail.com>
2022-03-05 09:18:36 +0100

    add data article

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -1,40 +1,9 @@
 import Article from './Article'
+import { articles } from './data.js'
 
 import './Articles.css'
 
-import SpaceStationImage from '../../Assets/SpaceStationFrontPage.png'
-import AstronautCloseFrontPage from '../../Assets/AstronautCloseFrontPage.png'
-import AstronautFrontPage from '../../Assets/AstronautFrontPage.png'
-import PlanetFrontPage from '../../Assets/PlanetFrontPage.png'
-
 export default function Articles() {
-	const articles = [
-		{
-			id: 1,
-			title: 'La station spatiale',
-			imageFront: SpaceStationImage,
-			content: '',
-			image: '',
-			photos: [],
-			numbers: '',
-		},
-		{
-			id: 2,
-			title: 'Les austronautes',
-			imageFront: AstronautCloseFrontPage,
-		},
-		{
-			id: 3,
-			title: "L'univers",
-			imageFront: PlanetFrontPage,
-		},
-		{
-			id: 4,
-			title: "Vivre à l'espace",
-			imageFront: AstronautFrontPage,
-		},
-	]
-
 	return (
 		<div className='articlesContainer'>
 			{articles.map(({ title, imageFront, id }) => {
---
+++ src/Components/Articles/data.js
@@ -0,0 +1,31 @@
+import SpaceStationImage from '../../Assets/SpaceStationFrontPage.png'
+import AstronautCloseFrontPage from '../../Assets/AstronautCloseFrontPage.png'
+import AstronautFrontPage from '../../Assets/AstronautFrontPage.png'
+import PlanetFrontPage from '../../Assets/PlanetFrontPage.png'
+
+export const articles = [
+	{
+		id: 1,
+		title: 'La station spatiale',
+		imageFront: SpaceStationImage,
+		content: '',
+		image: '',
+		photos: [],
+		numbers: '',
+	},
+	{
+		id: 2,
+		title: 'Les austronautes',
+		imageFront: AstronautCloseFrontPage,
+	},
+	{
+		id: 3,
+		title: "L'univers",
+		imageFront: PlanetFrontPage,
+	},
+	{
+		id: 4,
+		title: "Vivre à l'espace",
+		imageFront: AstronautFrontPage,
+	},
+]
