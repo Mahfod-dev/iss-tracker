3fca7d43a7a346fee84460ec18809c2185dc8d75
v-andreeva <veneta-andreeva_student2021@wilder.school>
2022-03-12 17:45:14 +0100

    work in progress do not merge

--- src/Components/Articles/ArticleItem.js
+++ src/Components/Articles/ArticleItem.js
@@ -2,18 +2,31 @@ import React from 'react'
 import { articles } from './data.js'
 import style from './Articles.module.css'
 import { useParams } from 'react-router-dom'
+import { MapContainer } from 'react-leaflet'
+
 const ArticleItem = () => {
 	const { id } = useParams()
 
 	const articleID = articles.find((article) => article.id === +id)
 	console.log(articleID)
-	const { title, imageFront } = articleID
-	return (
-		<div style={{ color: 'white' }}>
-			{title}
+	const { title, image, content, photos } = articleID
 
-			<img src={imageFront} alt='' />
-		</div>
+	return (
+		<main>
+		<article className={style.pageContainer}>
+			<div className={style.frame}>
+				<h1 className={style.pageTitle}>{title}</h1>
+				<p>{content}</p>
+				<div className={style.photos}>
+					<img src={photos[0]} alt='' className={style.photo} />
+					<img src={photos[1]} alt='' className={style.photo} />
+				</div>
+				<div className={style.numbers}></div>
+			</div>
+			
+			<img src={image} alt='' className={style.imageArticle}/>
+		</article>
+		</main>
 	)
 }
 
--- src/Components/Articles/data.js
+++ src/Components/Articles/data.js
@@ -1,31 +1,50 @@
 import SpaceStationImage from '../../Assets/SpaceStationFrontPage.png'
+import SpaceStation from '../../Assets/SpaceStation.png'
 import AstronautCloseFrontPage from '../../Assets/AstronautCloseFrontPage.png'
+import AstronautClose from '../../Assets/AstronautClose.png'
+import Astronaut from '../../Assets/Astronaut.png'
 import AstronautFrontPage from '../../Assets/AstronautFrontPage.png'
+import Planet from '../../Assets/Planet.png'
 import PlanetFrontPage from '../../Assets/PlanetFrontPage.png'
+import ISSphoto from '../../Assets/ISSphoto.jpeg'
+import insigne from '../../Assets/insigneISS.png'
+
 
 export const articles = [
 	{
 		id: 1,
 		title: 'La station spatiale',
 		imageFront: SpaceStationImage,
-		content: '',
-		image: '',
-		photos: [],
-		numbers: '',
+		content: 'La Station spatiale internationale, en abrégé SSI (surtout au Canada francophone) ou ISS (d\'après le nom anglais : International Space Station), est une station spatiale placée en orbite terrestre basse, occupée en permanence par un équipage international qui se consacre à la recherche scientifique dans l\'environnement spatial. Ce programme, lancé et piloté par la NASA, est développé conjointement avec l\'agence spatiale fédérale russe, avec la participation des agences spatiales européenne, japonaise et canadienne.',
+		image: SpaceStation,
+		photos: [ISSphoto,insigne],
+		numbers: '123',
 	},
 	{
 		id: 2,
 		title: 'Les austronautes',
 		imageFront: AstronautCloseFrontPage,
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis augue massa. Aliquam convallis nec nisl sit amet elementum. Praesent dapibus turpis id odio pellentesque aliquet. Sed maximus lorem sed leo pretium, at porttitor enim condimentum. Pellentesque ac sem laoreet, posuere nisi tincidunt, bibendum est. ',
+		image: AstronautClose,
+		photos: [],
+		numbers: '123',
 	},
 	{
 		id: 3,
 		title: "L'univers",
 		imageFront: PlanetFrontPage,
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis augue massa. Aliquam convallis nec nisl sit amet elementum. Praesent dapibus turpis id odio pellentesque aliquet. Sed maximus lorem sed leo pretium, at porttitor enim condimentum. Pellentesque ac sem laoreet, posuere nisi tincidunt, bibendum est. ',
+		image: Planet,
+		photos: [],
+		numbers: '',
 	},
 	{
 		id: 4,
 		title: "Vivre à l'espace",
 		imageFront: AstronautFrontPage,
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis augue massa. Aliquam convallis nec nisl sit amet elementum. Praesent dapibus turpis id odio pellentesque aliquet. Sed maximus lorem sed leo pretium, at porttitor enim condimentum. Pellentesque ac sem laoreet, posuere nisi tincidunt, bibendum est. ',
+		image: Astronaut,
+		photos: [],
+		numbers: '',
 	},
 ]
