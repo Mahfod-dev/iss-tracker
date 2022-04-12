a63e1e40714e5950c5de98616525dc0b58d8a596
Matthieu Mary <matthieu.mary2@gmail.com>
2022-04-12 21:31:10 +0200

    fix some footer css mistake

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import FooterForm from './FooterForm'
 import LegalNotice from './LegalNotice'
+import style from "./Footer.module.css"
 
 
 const Footer = () => {
 	return (
-		<footer>
+		<footer className={style.footer}>
 			<LegalNotice />
 			<FooterForm />
 		</footer>
