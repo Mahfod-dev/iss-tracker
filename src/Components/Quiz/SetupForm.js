c540e7ac08840436095f296b31bf9bb44c9eefec
Mahfod <mahfod.dev@gmail.com>
2022-04-14 19:19:11 +0200

    add quiz

--- src/Components/Quiz/SetupForm.js
+++ src/Components/Quiz/SetupForm.js
@@ -1,66 +1,67 @@
 import React from 'react'
 import { useContextQuiz } from '../../context/contextQuiz'
-import style from "./Quiz.module.css"
+import './Quiz.css'
 
 const SetupForm = () => {
 	const { quiz, handleChange, handleSubmit, error } = useContextQuiz()
 	return (
-		<main>
-			<section className={style['quiz quiz-small']}>
-				<form className={style['setup-form']}>
-					<h2>Setup quiz</h2>
-					<div className={style['form-control']}>
-						<label htmlFor='amount'>number of questions</label>
-						<input
-							type='number'
-							name='amount'
-							id='amount'
-							value={quiz.amount}
-							onChange={handleChange}
-							className={style['form-input']}
-							min={1}
-							max={50}
-						/>
-					</div>
-					{/* category */}
+		<section className='quiz quiz-small'>
+			<form className='setup-form'>
+				<h2>setup quiz</h2>
+				{/* amount */}
+				<div className='form-control'>
+					<label htmlFor='amount'>number of questions</label>
+					<input
+						type='number'
+						name='amount'
+						id='amount'
+						value={quiz.amount}
+						onChange={handleChange}
+						className='form-input'
+						min={1}
+						max={50}
+					/>
+				</div>
+				{/* category */}
 
-					<div className={style['form-control']}>
-						<label htmlFor='category'>category</label>
-						<select
-							name='category'
-							id='category'
-							className={style['form-input']}
-							value={quiz.category}
-							onChange={handleChange}>
-							<option value='science'>science</option>
-							<option value='mathematics'>mathematics</option>
-							<option value='gadgets'>sciences : gadgets</option>
-						</select>
-					</div>
-					{/* difficulty */}
+				<div className='form-control'>
+					<label htmlFor='category'>category</label>
+					<select
+						name='category'
+						id='category'
+						className='form-input'
+						value={quiz.category}
+						onChange={handleChange}>
+						<option value='sciences'>sciences</option>
+						<option value='mathematics'>mathematics</option>
+						<option value='gadgets'>gadgets</option>
+					</select>
+				</div>
+				{/* difficulty */}
 
-					<div className={style['form-control']}>
-						<label htmlFor='difficulty'>select difficulty</label>
-						<select
-							name='difficulty'
-							id='difficulty'
-							className={style['form-input']}
-							value={quiz.difficulty}
-							onChange={handleChange}>
-							<option value='easy'>easy</option>
-							<option value='medium'>medium</option>
-							<option value='hard'>hard</option>
-						</select>
-					</div>
-					{error && (
-						<p className={style.error}>Can't generate questions,please try different options</p>
-					)}
-					<button className={style['submit-btn']} type='submit' onClick={handleSubmit}>
-						Start
-					</button>
-				</form>
-			</section>
-		</main>
+				<div className='form-control'>
+					<label htmlFor='difficulty'>select difficulty</label>
+					<select
+						name='difficulty'
+						id='difficulty'
+						className='form-input'
+						value={quiz.difficulty}
+						onChange={handleChange}>
+						<option value='easy'>easy</option>
+						<option value='medium'>medium</option>
+						<option value='hard'>hard</option>
+					</select>
+				</div>
+				{error && (
+					<p className='error'>
+						can't generate questions, please try different options
+					</p>
+				)}
+				<button type='submit' onClick={handleSubmit} className='submit-btn'>
+					start
+				</button>
+			</form>
+		</section>
 	)
 }
 
