f90b2c95c3305507828a0f59303e2c324ffa132e
Mahfod <mahfod.dev@gmail.com>
2022-04-16 03:33:07 +0200

    comment not use img

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -3,10 +3,10 @@ import { articles } from './data.js'
 
 import style from './Articles.module.css'
 
-import SpaceStationImage from '../../Assets/SpaceStationFrontPage.png'
-import AstronautCloseFrontPage from '../../Assets/AstronautCloseFrontPage.png'
-import AstronautFrontPage from '../../Assets/AstronautFrontPage.png'
-import PlanetFrontPage from '../../Assets/PlanetFrontPage.png'
+// import SpaceStationImage from '../../Assets/SpaceStationFrontPage.png'
+// import AstronautCloseFrontPage from '../../Assets/AstronautCloseFrontPage.png'
+// import AstronautFrontPage from '../../Assets/AstronautFrontPage.png'
+// import PlanetFrontPage from '../../Assets/PlanetFrontPage.png'
 
 export default function Articles() {
 	return (
